c00d770f6c5ab133d5823e44d4ce06011806544c
Jelena <jeksiko@gmail.com>
2022-06-22 20:36:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function googleIcon(url) {
+    window.open("https://www.google.rs/",'_blank');
+}
+
+function facebookIcon(url) {
+    window.open("https://www.facebook.com/",'_blank');
+}
+
+function appleIcon(url){
+    window.open("https://www.apple.com/",'_blank');
+}
+
+function twitterIcon(url) {
+    window.open("https://twitter.com/", '_blank');
+}
